579461780efd0df751de77bf0f94b02298509dfd
mindheist <dragonzsnake@gmail.com>
2015-04-01 00:18:56 -0700

    + Values and Variables code

---
+++ values.go
@@ -0,0 +1,21 @@
+package main 
+
+import "fmt"
+
+func main () {
+
+
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 = " , 1 + 1)
+
+	fmt.Println ("7.0 / 3.0 =" , 7.0/3.0 )
+
+	fmt.Println( "true and False = " , true && false)
+
+	fmt.Println ( "true or false ==" , true || false)
+
+	fmt.Println( "not of true = " ,  !true)
+
+
+}
---
+++ variables.go
@@ -0,0 +1,25 @@
+package main 
+
+import "fmt"
+
+func main () {
+
+	var a string = "initial"
+	fmt.Println("a = " , a)
+
+	var b , c int = 1 , 2
+	fmt.Println("b,c =" , b,c)
+
+	var d = true
+
+	fmt.Println("d = ", d)
+
+	var e int
+	fmt.Println("e = " , e)
+
+
+	f:= "just_another_string"
+	fmt.Println("f ==" , f)
+
+
+}
